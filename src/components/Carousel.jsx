d6ee1602e1658a0d8d79087d0f41be102b8323fe
Tommmychinn <tommychinn1@gmail.com>
2020-07-07 15:14:04 +0100

    toastify final changes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,51 +4,50 @@ import { Container, Row, Table, Spinner } from "reactstrap";
 import axios from "axios";
 
 const Carousel = () => {
-    const [carouselDatas, setCarouselDatas] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [carouselDatas, setCarouselDatas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const getCarousel = async () => {
-            try {
-                const res = await axios.get(
-                    "https://btz-js-202003-p3-lookup-back.jsrover.wilders.dev/carousels/"
-                );
-                setCarouselDatas(res.data);
-            } catch (err) {
-                console.log(err);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-        getCarousel();
-    }, []);
+  useEffect(() => {
+    const getCarousel = async () => {
+      try {
+        const res = await axios.get(
+          "https://btz-js-202003-p3-lookup-back.jsrover.wilders.dev/carousels/"
+        );
+        setCarouselDatas(res.data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getCarousel();
+  }, []);
 
-    if (isLoading) {
-        return <Spinner color="primary" />;
-    }
+  if (isLoading) {
+    return <Spinner color="primary" />;
+  }
 
-    return (
-        <Container>
-            <Row>
-                <h1>Carousel</h1>
-            </Row>
-            <Row>
-                <Table>
-                    {carouselDatas.map((it) => (
-                        <BaseCardCarousel
-                            key={it.uuid}
-                            uuid={it.uuid}
-                            titre={it.title}
-                            link={it.link}
-                            picture={it.picture}
-                            descriptif={it.description}
-                            onClick={() => alert("coucou")}
-                        />
-                    ))}
-                </Table>
-            </Row>
-        </Container>
-    );
+  return (
+    <Container>
+      <Row>
+        <h1>Carousel</h1>
+      </Row>
+      <Row>
+        <Table>
+          {carouselDatas.map((it) => (
+            <BaseCardCarousel
+              key={it.uuid}
+              uuid={it.uuid}
+              titre={it.title}
+              link={it.link}
+              picture={it.picture}
+              descriptif={it.description}
+            />
+          ))}
+        </Table>
+      </Row>
+    </Container>
+  );
 };
 
 export default Carousel;
--- src/components/builders/ModalCarousel.jsx
+++ src/components/builders/ModalCarousel.jsx
@@ -3,201 +3,201 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useForm } from "react-hook-form";
 import {
-    Form,
-    Button,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Container,
-    Row,
-    Col,
+  Form,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Container,
+  Row,
+  Col,
 } from "reactstrap";
 import Axios from "axios";
 
 toast.configure();
 const ModalCarousel = ({
-    onClick,
+  onClick,
+  title,
+  description,
+  link,
+  picture,
+  uuid,
+}) => {
+  const notifySuccess = () => {
+    toast.success("Carousel bien modifié !", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const notifyError = () => {
+    toast.error("Erreur Notification !", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const [modal, setModal] = useState(false);
+
+  const [carousel, setCarousel] = useState({
     title,
     description,
     link,
     picture,
-    uuid,
-}) => {
-    const notifySuccess = () => {
-        toast.success("Carousel bien modifié !", {
-            position: "bottom-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-        });
-    };
-    const notifyError = () => {
-        toast.error("Erreur Notification !", {
-            position: "bottom-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-        });
-    };
-    const [modal, setModal] = useState(false);
+  });
+  const { handleSubmit, register } = useForm();
+  // const onSubmit = (values) => console.log(values);
 
-    const [carousel, setCarousel] = useState({
-        title,
-        description,
-        link,
-        picture,
-    });
-    const { handleSubmit, register, errors } = useForm();
-    // const onSubmit = (values) => console.log(values);
+  const toggle = () => setModal(!modal);
 
-    const toggle = () => setModal(!modal);
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEwODAzMjkyLWE4YTgtNGVlYy04OTViLTliODlmYzk1OWY0ZiIsImVtYWlsIjoiYW50aG9uaW42NEBsb29rdXAuZnIiLCJpYXQiOjE1OTQxMzA5ODAsImV4cCI6MTU5NDEzNDU4MH0.SXeCaMA-8mufsHNsKlL2Sae6rH-dcP5gZfrpvjwKtF0";
 
-    const token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEwODAzMjkyLWE4YTgtNGVlYy04OTViLTliODlmYzk1OWY0ZiIsImVtYWlsIjoiYW50aG9uaW42NEBsb29rdXAuZnIiLCJpYXQiOjE1OTQxMjcxMzQsImV4cCI6MTU5NDEzMDczNH0.mgJ0fcy2e3MKPpB_SThS28vrVsyfiQT7Te5cCp2PH3Q";
-
-    const putCarousel = async () => {
-        try {
-            await Axios.put(
-                `https://btz-js-202003-p3-lookup-back.jsrover.wilders.dev/carousels/${uuid}`,
-                carousel,
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            );
-            notifySuccess();
-        } catch (err) {
-            notifyError();
-            console.log(err);
+  const putCarousel = async () => {
+    try {
+      await Axios.put(
+        `https://btz-js-202003-p3-lookup-back.jsrover.wilders.dev/carousels/${uuid}`,
+        carousel,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-    };
+      );
+      notifySuccess();
+    } catch (err) {
+      notifyError();
+      console.log(err);
+    }
+  };
 
-    return (
-        <Container>
-            <Button color="danger" onClick={toggle}>
-                Modifier
+  return (
+    <Container>
+      <Button color="danger" onClick={toggle}>
+        Modifier
+      </Button>
+
+      <Modal isOpen={modal} toggle={toggle} size="lg">
+        <ModalHeader toggle={toggle}>Carousel</ModalHeader>
+        <Form onSubmit={handleSubmit(putCarousel)}>
+          <ModalBody>
+            <Row>
+              <Col lg="12">
+                <label>Titre </label>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="6">{title}</Col>
+              <Col lg="6">
+                <input
+                  ref={register({ required: true })}
+                  type="text"
+                  name="title"
+                  onChange={(e) =>
+                    setCarousel({
+                      ...carousel,
+                      title: e.target.value,
+                    })
+                  }
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="12">
+                <label> Description</label>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="6">{description}</Col>
+              <Col lg="6">
+                <input
+                  ref={register({ required: true })}
+                  name="description"
+                  type="text"
+                  onChange={(e) =>
+                    setCarousel({
+                      ...carousel,
+                      description: e.target.value,
+                    })
+                  }
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="12">
+                <label>Lien </label>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="6">{link}</Col>
+              <Col lg="6">
+                <input
+                  ref={register({ required: true })}
+                  type="text"
+                  name="lien"
+                  onChange={(e) =>
+                    setCarousel({
+                      ...carousel,
+                      link: e.target.value,
+                    })
+                  }
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="12">
+                <label> Image</label>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="6">{picture}</Col>
+              <Col lg="6">
+                <input
+                  ref={register({ required: true })}
+                  type="text"
+                  name="image"
+                  onChange={(e) =>
+                    setCarousel({
+                      ...carousel,
+                      picture: e.target.value,
+                    })
+                  }
+                />
+              </Col>
+            </Row>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={onClick}>
+              Valider
+            </Button>{" "}
+            <Button color="secondary" onClick={toggle}>
+              Annuler
             </Button>
-
-            <Modal isOpen={modal} toggle={toggle} size="lg">
-                <ModalHeader toggle={toggle}>Carousel</ModalHeader>
-                <Form onSubmit={handleSubmit(putCarousel)}>
-                    <ModalBody>
-                        <Row>
-                            <Col lg="12">
-                                <label>Titre </label>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="6">{title}</Col>
-                            <Col lg="6">
-                                <input
-                                    ref={register({ required: true })}
-                                    type="text"
-                                    name="title"
-                                    onChange={(e) =>
-                                        setCarousel({
-                                            ...carousel,
-                                            title: e.target.value,
-                                        })
-                                    }
-                                />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="12">
-                                <label> Description</label>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="6">{description}</Col>
-                            <Col lg="6">
-                                <input
-                                    ref={register({ required: true })}
-                                    name="description"
-                                    type="text"
-                                    onChange={(e) =>
-                                        setCarousel({
-                                            ...carousel,
-                                            description: e.target.value,
-                                        })
-                                    }
-                                />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="12">
-                                <label>Lien </label>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="6">{link}</Col>
-                            <Col lg="6">
-                                <input
-                                    ref={register({ required: true })}
-                                    type="text"
-                                    name="lien"
-                                    onChange={(e) =>
-                                        setCarousel({
-                                            ...carousel,
-                                            link: e.target.value,
-                                        })
-                                    }
-                                />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="12">
-                                <label> Image</label>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col lg="6">{picture}</Col>
-                            <Col lg="6">
-                                <input
-                                    ref={register({ required: true })}
-                                    type="text"
-                                    name="image"
-                                    onChange={(e) =>
-                                        setCarousel({
-                                            ...carousel,
-                                            picture: e.target.value,
-                                        })
-                                    }
-                                />
-                            </Col>
-                        </Row>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="primary" onClick={toggle}>
-                            Valider
-                        </Button>{" "}
-                        <Button color="secondary" onClick={toggle}>
-                            Annuler
-                        </Button>
-                    </ModalFooter>
-                </Form>
-            </Modal>
-            <ToastContainer
-                position="bottom-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-        </Container>
-    );
+          </ModalFooter>
+        </Form>
+      </Modal>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Container>
+  );
 };
 
 export default ModalCarousel;
