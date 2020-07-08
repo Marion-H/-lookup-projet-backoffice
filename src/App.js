076479aa17ead67f134afa1102c9dd1f923ec905
Adèlebp <abobinparra@gmail.com>
2020-07-08 11:47:36 +0200

    form created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./App.css";
+import Login from "./components/Login";
 import Router from "./components/Router/Router";
 
+const token = false;
 function App() {
-  return (
-    <>
-      <Router />
-    </>
-  );
+  if (token) {
+    return (
+      <>
+        <Router />
+      </>
+    );
+  } else {
+    return <Login />;
+  }
 }
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,87 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useForm } from "react-hook-form";
+
+import { Form, Button, Container, Row, Col } from "reactstrap";
+// import Axios from 'axios';
+
+toast.configure();
+const Login = () => {
+  const { handleSubmit, register, errors } = useForm();
+  const onSubmit = (values) => console.log(values);
+  const notifySuccess = () => {
+    toast.success("Bienvenue !", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const notifyError = () => {
+    toast.error("Email ou password incorrect !", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Login</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Col>
+            <input
+              name="email"
+              ref={register({
+                required: "Required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "invalid email address",
+                },
+              })}
+            />
+            {errors.email && errors.email.message}
+          </Col>
+          <Col>
+            <input
+              name="username"
+              ref={register({
+                validate: (value) => value !== "admin" || "Nice try!",
+              })}
+            />
+            {errors.username && errors.username.message}
+          </Col>
+          <Button color="success" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Row>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Container>
+  );
+};
+
+export default Login;
