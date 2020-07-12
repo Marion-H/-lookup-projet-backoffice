2e14e03ece61236e2e116e9f64a4896ea204a7b0
Marion-H <doudou6500@gmail.com>
2020-07-12 19:18:55 +0200

    add function isAuthenticated in component modalPress and addPress. use Dispatch in component deletePresse

--- src/components/builders/DeletePress.jsx
+++ src/components/builders/DeletePress.jsx
@@ -3,9 +3,13 @@ import { Button, Col } from "reactstrap";
 import Axios from "axios";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+
+import { logout } from "../../store/actionCreators";
 
 export default function DeletePress({ uuid, getPress }) {
   const token = useSelector((state) => state.admin.token);
+  const dispatch = useDispatch();
 
   const notifySuccess = () => {
     toast.success("Relation Presse supprimée!", {
@@ -44,6 +48,7 @@ export default function DeletePress({ uuid, getPress }) {
       notifySuccess();
     } catch (error) {
       notifyError();
+      dispatch(logout());
     }
   };
   return (
